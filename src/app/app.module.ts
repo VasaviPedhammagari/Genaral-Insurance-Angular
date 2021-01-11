294b46e02c85b48420238dd43e943c97d7a03ffc
RajPriyaB <b.priyarenu@gmail.com>
2021-01-11 09:02:20 +0530

    added needed imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
+import { FormsModule, ReactiveFormsModule  } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
-import { RegisterComponent } from './register/register.component';
 import { BuyInsuranceComponent } from './buy-insurance/buy-insurance.component';
+import { CustomFormsModule } from 'ng2-validation';
+
+
+ 
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    RegisterComponent,
-    BuyInsuranceComponent
+    BuyInsuranceComponent,
+    routingComponents
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    CustomFormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
