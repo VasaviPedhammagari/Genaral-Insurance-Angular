c65d3ff823d41c26390ceb3931547f524c47d086
RajPriyaB <b.priyarenu@gmail.com>
2021-01-11 09:01:20 +0530

    added components in routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { VehicleComponent } from './vehicle/vehicle.component';
+import { RegisterComponent } from './register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'register', component: RegisterComponent },
+  { path: 'vehicle-registration', component: VehicleComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const routingComponents = [RegisterComponent, VehicleComponent]
