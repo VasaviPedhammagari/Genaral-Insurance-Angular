b786711c262b8d4c4ccb18274dec1d43ce5082f7
RajPriyaB <b.priyarenu@gmail.com>
2021-01-11 08:54:03 +0530

    created class for user

---
+++ src/app/appmodel/user.ts
@@ -0,0 +1,12 @@
+export class User{
+  username: string;
+  email: string;
+  dateOfBirth:string;
+  phoneno:number;
+  street:string;
+  city:string;
+  pincode:string;
+  state:string;
+  password1:string;
+  password2:string;
+}
