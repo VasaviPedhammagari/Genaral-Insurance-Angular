7e129d9077b9e45dc584569f08b8965a29e899de
RajPriyaB <b.priyarenu@gmail.com>
2021-01-11 08:58:45 +0530

    adding validations and funcitons for vehicle registration(not completed)

---
+++ src/app/vehicle/vehicle.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
+import { Vehicle } from '../appmodel/vehicle';
+import { InsuranceService } from '../insurance.service';
+
+@Component({
+  selector: 'app-vehicle',
+  templateUrl: './vehicle.component.html',
+  styleUrls: ['./vehicle.component.css']
+})
+export class VehicleComponent implements OnInit {
+
+  VehicleForm:FormGroup;
+  vehicle:Vehicle = new Vehicle();
+
+  constructor(private fb: FormBuilder,private insuranceService: InsuranceService) { }
+
+  ngOnInit(): void {
+    this.VehicleForm = this.fb.group({
+      manufacturer: ["",Validators.required],
+      model: ["",Validators.required],
+      license: ["",Validators.required],
+      purchaseDate: ["",Validators.required],
+      regno: ["",Validators.required],
+      engineno: ["",Validators.required],
+      chassisno: ["",Validators.required]
+    })
+  }
+
+  saveVehicle(){
+    console.log("saveVehicle working!");
+    
+  }
+
+}
