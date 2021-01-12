3c33400301ca76de708beb6a2398675f915e7f01
RajPriyaB <b.priyarenu@gmail.com>
2021-01-12 11:58:33 +0530

    added routing in user registration

--- src/app/insurance.service.ts
+++ src/app/insurance.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Login } from './appmodel/login';
 import { User } from './appmodel/user';
+import { Vehicle } from './appmodel/vehicle';
 
 @Injectable({
   providedIn: 'root'
@@ -18,5 +19,10 @@ export class InsuranceService {
   login(login: Login) : Observable<any>{
     let url = "http://localhost:8181/login";
     return this.http.post(url, login);  
+  }
+  registerVehicle(vehicle: Vehicle) :Observable<any> {
+    console.log("registerVehicle working!");
+    let url = "http://localhost:8181/register-vehicle";
+    return this.http.post(url, vehicle);
   }
 }
--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -4,6 +4,7 @@ import { CustomValidators } from 'ng2-validation';
 import { Address } from '../appmodel/address';
 import { User } from '../appmodel/user';
 import { InsuranceService } from '../insurance.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -15,14 +16,15 @@ export class RegisterComponent implements OnInit {
   RegForm:FormGroup;
   user:User = new User();
   address:Address = new Address();
+  message: string;
 
-  constructor(private fb: FormBuilder, private insuranceService: InsuranceService) {
+  constructor(private fb: FormBuilder, private insuranceService: InsuranceService, private router: Router) {
    }
 
   public ngOnInit(): void {
     this.RegForm = this.fb.group({
       username: ["",Validators.required],
-      email: ["",Validators.compose([Validators.required, CustomValidators.email])],
+      email: ["",Validators.compose([Validators.required, Validators.pattern(/^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/)])],
       dateOfBirth: ["",Validators.required],
       phoneno: ["",Validators.compose([Validators.required, Validators.pattern(/^\d{10}$/)])],
       street: ["",Validators.required],
@@ -31,7 +33,7 @@ export class RegisterComponent implements OnInit {
       pincode: ["",Validators.compose([Validators.required,Validators.pattern(/^\d{6}$/)])],
       password1: ["",Validators.compose([
           Validators.required,
-          Validators.pattern('(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&].{8,}')
+          Validators.pattern('(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&].{8,12}')
          ])
       ],
       password2: ["",Validators.required]
@@ -40,28 +42,26 @@ export class RegisterComponent implements OnInit {
 
   register(){
     //console.log("Register function works!");
-<<<<<<< HEAD
-    if(this.user.password == this.password2){
-      console.log(this.password2);
-      alert(JSON.stringify(this.user));
-=======
     if(this.user.password == this.user.password2){
       //alert(JSON.stringify(this.user));
+  
       this.user.address = this.address;
->>>>>>> 6cc1d1a674f43fa247873bca3872fcf9fd52e7ce
       this.insuranceService.registerUser(this.user).subscribe(response => {
         alert(JSON.stringify(response));
+        if(response.status == 'SUCCESS') {
+          let userId = response.userId;
+          let userName = response.userName;
+          sessionStorage.setItem('userId', String(userId));
+          sessionStorage.setItem('userName', userName);
+          this.router.navigate(['vehicle-registration']);
+        }
+        else
+          this.message = response.message;
     })
     }
     else{
       alert("Password mismatch!");
     }
-  } 
-}
-export class Address{
-  addressId:number;
-	addressLine:string;
-	city:string;
-	pin:number;
-	state:number;
+  }
+  
 }
