3f9b37fec43a9ca6745a0b04741cdb778b45fdd5
VasaviReddy <vasavireddyp1998@gmail.com>
2021-01-15 17:47:14 +0530

    login to renew

--- src/app/login-profile/login-profile.component.ts
+++ src/app/login-profile/login-profile.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { MotorInsurance } from '../appmodel/motorInsurance';
+import { User } from '../appmodel/user';
 import { InsuranceService } from '../insurance.service';
 
 @Component({
@@ -15,20 +16,36 @@ export class LoginProfileComponent implements OnInit {
   userId : number;
   userName : string;
   motorInsuranceList : MotorInsurance[];
-  policyNumberList : Array<number>;
+  policyNumberList : number[];
   length:number;
+  user: User;
 
   ngOnInit(): void {
     this.userId = parseInt(sessionStorage.getItem('userId') || '{}');
     this.userName = sessionStorage.getItem('userName') || '{}';
     this.insuranceService.insuranceDetails(this.userId).subscribe(response => {
       this.motorInsuranceList = response;
+      var sortedArray: MotorInsurance[] = this.motorInsuranceList.sort((obj1, obj2) => {​​
+        if (obj1.policyNumber < obj2.policyNumber) {​​
+            return 1;
+        }​​
+        if (obj1.policyNumber > obj2.policyNumber) {​​
+            return -1;
+        }​​
+        return 0;
+     }​​);
       this.length = this.motorInsuranceList.length;
       this.policyNumberList = new Array<number>(this.motorInsuranceList.length);
       for(var i =0; i < this.length;i++){
         console.log(JSON.stringify(this.motorInsuranceList[i]));
-        this.policyNumberList.push(parseInt(this.motorInsuranceList[i].policyNumber));
+        this.policyNumberList[i]=parseInt(this.motorInsuranceList[i].policyNumber);
+        this.user = this.motorInsuranceList[i].user;
       }
+      console.log(JSON.stringify(this.user));
+      sessionStorage.setItem('policyNumbers',JSON.stringify(this.policyNumberList));
+      sessionStorage.setItem('email',this.user.email);
+      sessionStorage.setItem('mobileNumber',String(this.user.phoneNo));
+      console.log(JSON.stringify(this.policyNumberList));
     })
   }
 
