d20c6ededd102f70242b0b2f6042fa054b4147dc
Rutvik <kulkarnirutvikvishwas@gmail.com>
2021-01-18 12:54:12 +0530

    Filter in buy insurance

--- src/app/insurance-plan/insurance-plan.component.ts
+++ src/app/insurance-plan/insurance-plan.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Estimate } from '../appmodel/estimate';
 import { MotorInsurance } from '../appmodel/motorInsurance';
 import { User } from '../appmodel/user';
 import { Vehicle } from '../appmodel/vehicle';
@@ -15,22 +16,44 @@ export class InsurancePlanComponent implements OnInit {
   motorInsurance: MotorInsurance = new MotorInsurance();
   vehicle: Vehicle = new Vehicle();
   user: User = new User();
+  estimate: Estimate[];
+  appliedEstimates: Estimate[];
+  type: string = '';
+  years: string = '';
 
   constructor(private router: Router, private insuranceService: InsuranceService) { }
 
   ngOnInit(): void {
     this.vehicle = JSON.parse(sessionStorage.getItem('vehicle') || '{}');
     this.user = JSON.parse(sessionStorage.getItem('user') || '{}');
-    alert('in insurance');
+    alert(JSON.stringify(this.user));
+    this.estimate = JSON.parse(sessionStorage.getItem('estimateBuyInsurance') || '{}');
+    this.appliedEstimates = this.estimate;
   }
 
-  saveData(data: any) {
-    this.motorInsurance.planType = data.plan;
-    this.motorInsurance.noOfYrs = data.planTerm;
+  onSubmit(buttonType: any): void {
+    if (buttonType === "apply") {
+      this.appliedEstimates = [];
+      for (var i = 0; i < this.estimate.length; i++) {
+        if (this.estimate[i].type === this.type && this.estimate[i].noOfYears === parseInt(this.years)) {
+          this.appliedEstimates.push(this.estimate[i]);
+        }
+      }
+    }
+    if (buttonType === "clear") {
+      this.type = '';
+      this.years = '';
+      this.appliedEstimates = this.estimate;
+    }
+
+  }
+
+  onClickPrice(type: string, price: number, coverage: number, noOfYears: number) {
+    this.motorInsurance.planType = type;
+    this.motorInsurance.noOfYrs = noOfYears;
     this.motorInsurance.vehicle = this.vehicle;
     this.motorInsurance.user = this.user;
-    alert('inside save data');
-    console.log(JSON.stringify(this.motorInsurance));
+    alert('user in motot'+JSON.stringify(this.motorInsurance.user));
     this.insuranceService.choosePlan(this.motorInsurance).subscribe(response => {
       console.log(JSON.stringify(response));
       if (response.status == 'SUCCESS') {
@@ -43,4 +66,5 @@ export class InsurancePlanComponent implements OnInit {
         alert(response.message);
     })
   }
+
 }
